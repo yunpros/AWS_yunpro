87f1ac590fe3af5a996f1a9f699a0feae4070227
root <root@ip-172-31-47-102.ap-northeast-2.compute.internal>
2022-05-03 05:44:33 +0000

    second commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-0ed11f3863410c386"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Trraform_yunpro"
+  }
+}
+
